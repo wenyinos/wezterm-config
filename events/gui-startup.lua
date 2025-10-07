d2a9f0e58c0ab9e3faf20fc550b36def60a573d0
Wenyin Root <ruojiner@hotmail.com>
2025-10-07 14:52:58 +0800

    ``` feat(events): 修改gui启动时的窗口尺寸设置
    将原先最大化窗口的行为改为设置固定的内尺寸为80列24行，
    并注释掉webgpu_power_preference配置项
    ```

--- events/gui-startup.lua
+++ events/gui-startup.lua
@@ -6,8 +6,9 @@ local M = {}
 M.setup = function()
    wezterm.on('gui-startup', function(cmd)
       local _, _, window = mux.spawn_window(cmd or {})
-      window:gui_window():maximize()
+      local gui_window = window:gui_window()
+      gui_window:set_inner_size(80, 24)
    end)
 end
-
+--config.webgpu_power_preference = "LowPower"
 return M
